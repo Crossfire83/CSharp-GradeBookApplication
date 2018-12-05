6e48ad8363b4f9aece0f4e02dd5f85d096547708
perezrau <perezrau@PEREZRAU2>
2018-12-05 13:59:35 -0600

    .

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -49,7 +49,7 @@ namespace GradeBook.UserInterfaces
                     gradeBook = new RankedGradeBook(name);
                     break;
                 default:
-                    Console.WriteLine("{0}  is not a supported type of gradebook, please try again", type);
+                    Console.WriteLine("{0} is not a supported type of gradebook, please try again", type);
                     return;
             }
 
